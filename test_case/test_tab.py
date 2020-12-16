19e39d59a6ada338b4ce2ca07f1474c20e71cf5d
kotera <koteramomo@gmail.com>
2020-12-16 18:21:54 +0800

    tablist case

--- test_case/test_tab.py
+++ test_case/test_tab.py
@@ -7,10 +7,10 @@ class tabListRequest():
         endpoint = '/api/asset/listPageQueryTaskByStatus.json?'
         tab_list = (
             "statusListStr=200,300,400,600,500,700,800,900,1000,1100&pageSize=15&currentPage=1",  # 全部
-            # "cs2 = 'statusListStr=200&pageSize=15&currentPage=1'",  # 待分配
-            # "cs3 = 'statusListStr=300&pageSize=15&currentPage=1'",  #待执行
-            # "cs4 = 'statusListStr=400,600,500,700&pageSize=15&currentPage=1'"   #执行中
-            # "cs5 = 'statusListStr=800,900,1000,1100&pageSize=15&currentPage=1'"   #已结束
+            "cs2 = 'statusListStr=200&pageSize=15&currentPage=1'",  # 待分配
+            "cs3 = 'statusListStr=300&pageSize=15&currentPage=1'",  #待执行
+            "cs4 = 'statusListStr=400,600,500,700&pageSize=15&currentPage=1'"   #执行中
+            "cs5 = 'statusListStr=800,900,1000,1100&pageSize=15&currentPage=1'"   #已结束
         )
         url_tab = ''.join([host, endpoint,tab_list[0]])
         print(url_tab)
