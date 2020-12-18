46526c9f9f1184f17d4cd9e71cb1823c6db270b9
kotera <koteramomo@gmail.com>
2020-12-18 17:33:55 +0800

    新增list_url

--- conf/config.py
+++ conf/config.py
@@ -1,3 +1,4 @@
+
 import requests,unittest
 
 class userConfig:
@@ -8,8 +9,9 @@ class userConfig:
 
 class urlConfig:
     def base_url(self):
-        url = 'https://iov-asset-protection.prepub.souche-inc.com/'
-        return url
+        global host
+        host = 'https://iov-asset-protection.prepub.souche-inc.com/'
+        return host
 
     def login_url(self):
         host = urlConfig.base_url(1)
@@ -17,7 +19,27 @@ class urlConfig:
         url_login = ''.join([host, endpoint]) # 登录接口路径
         return url_login
 
+    def list_url(self):
+        endpoint = 'api/asset/listPageQueryTaskByStatus.json'
+        url_list = ''.join([host,endpoint])
+        return url_list
 
 class commonUsed:
     def case_endpoint(self):
         a = '*****'
+
+'''
+
+[host]
+host=https://iov-asset-protection.prepub.souche-inc.com/
+
+[url]
+login_url=login.json
+list_url=api/asset/listPageQueryTaskByStatus.json
+
+
+[user]
+userAccount=17376509487
+password=pw
+
+'''
