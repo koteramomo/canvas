671e1bbb833a1c433a45c33906e54507e285480b
zhoulimin <zhoulimin@souche.com>
2021-05-26 17:53:38 +0800

    host变更

--- conf/config.py
+++ conf/config.py
@@ -1,11 +1,14 @@
 
-import requests,unittest
+import requests
+import unittest
+
 
 class userConfig:
     def user_data(self):
         pw = input("输入密码：")
-        user_kotera = {"userAccount":"17376509487","password":pw}
+        user_kotera = {"userAccount": "17376509487", "password": pw}
         return user_kotera
+
 
 class urlConfig:
     def base_url(self):
@@ -16,22 +19,26 @@ class urlConfig:
     def login_url(self):
         host = urlConfig.base_url(1)
         endpoint = 'login.json'
-        url_login = ''.join([host, endpoint]) # 登录接口路径
+        url_login = ''.join([host, endpoint])  # 登录接口路径
         return url_login
 
     def list_url(self):
         endpoint = 'api/asset/listPageQueryTaskByStatus.json'
-        url_list = ''.join([host,endpoint])
+        url_list = ''.join([host, endpoint])
         return url_list
+
 
 class reqHeader:
     def fill_header(self):
         header = {
 
         }
+
+
 class commonUsed:
     def case_endpoint(self):
         a = '*****'
+
 
 '''
 
